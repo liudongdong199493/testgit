15a1d46a9809d03b1cad5b8a70763680ad7845ce
ldd <2282703625@qq.com>
2018-11-26 14:53:23 +0800

    lombok的使用

--- src/main/java/com/baizhi/testgit/entity/User.java
+++ src/main/java/com/baizhi/testgit/entity/User.java
@@ -1,9 +1,17 @@
 package com.baizhi.testgit.entity;
 
+import lombok.Data;
+
+@Data
 public class User {
     private String username;
     private String password;
 
+    public static void main(String[] args) {
+        User user = new User();
+        user.setUsername("llll");
+        user.getUsername();
+    }
 
 
 }

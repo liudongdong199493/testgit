4ea2efbb62fa9c04181d75b69219c971b6ee1179
ldd <2282703625@qq.com>
2018-11-26 08:51:52 +0800

    第二次提交

--- src/main/java/com/baizhi/testgit/TestgitApplication.java
+++ src/main/java/com/baizhi/testgit/TestgitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestgitApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TestgitApplication.class, args);
     }
 }
---
+++ src/main/java/com/baizhi/testgit/entity/User.java
@@ -0,0 +1,4 @@
+package com.baizhi.testgit.entity;
+
+public class User {
+}
